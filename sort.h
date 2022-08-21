33974fb5547e03ef13daec926beba8a97f8ad364
mgmtsweni <mtswenigift@gmail.com>
2022-08-21 02:00:28 +0200

    update

--- sort.h
+++ sort.h
@@ -7,7 +7,6 @@
 
 /**
  * struct listint_s - Doubly linked list node
- *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -20,8 +19,27 @@ typedef struct listint_s
 } listint_t;
 
 
-/* print.c */
+/* given  */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/* mandatory */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi);
+size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi);
+void swap(int *array, size_t size, int *a, int *b)
+
+
+/*advance
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);*/
 #endif
